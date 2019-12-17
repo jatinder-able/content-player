cdde9364c017baff295cc926ff4f9925c88d885a
techkrishna <krishinbox2@gmail.com>
2019-12-17 18:51:25 +0530

    Debugging escape button issue.

--- player/public/coreplugins/org.ekstep.pdfrenderer-1.0/renderer/plugin.js
+++ player/public/coreplugins/org.ekstep.pdfrenderer-1.0/renderer/plugin.js
@@ -228,7 +228,16 @@ org.ekstep.contentrenderer.baseLauncher.extend({
             }
             $(this).hide().prev('span').show();
         });
+        $(document).keydown(function() {
+            console.log("Keydown Event");
+        });
+        $(document).keypress(function() {
+            console.log("Keypress Event");
+        });
         document.addEventListener("keydown", closeFullScreen, false);
+        document.documentElement.addEventListener("keydown", closeFullScreen, false);
+        document.addEventListener("keypress", closeFullScreen, false);
+        document.documentElement.addEventListener("keypress", closeFullScreen, false);
         function closeFullScreen(e) {
             console.log("Escape Button");
             console.log(e);
