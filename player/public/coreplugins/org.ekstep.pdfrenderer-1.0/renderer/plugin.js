588636eb7de94ad5a7e5af896eddf02a1113d945
techkrishna <krishinbox2@gmail.com>
2019-12-18 22:59:37 +0530

    Full Screen code has been moved from pdf renderer to index.html

--- player/public/coreplugins/org.ekstep.pdfrenderer-1.0/renderer/plugin.js
+++ player/public/coreplugins/org.ekstep.pdfrenderer-1.0/renderer/plugin.js
@@ -197,37 +197,37 @@ org.ekstep.contentrenderer.baseLauncher.extend({
 
 
         // Full Screen Icon Added
-        $("#pdf-fullscreen-container").html('<span title="Full Screen" id="able-fullscreen">' +
-                                                '<img src="./assets/fullscreen.png">' +
-                                            '</span>' +
-                                            '<span title="Exit Full Screen" id="able-exit-fullscreen">' +
-                                                '<img src="./assets/exit-fullscreen.png">' +
-                                            '</span>');
-        var elem = document.documentElement;
-        $("#pdf-fullscreen-container").on('click','#able-fullscreen',function() {
-            if (elem.requestFullscreen) {
-                elem.requestFullscreen();
-            } else if (elem.mozRequestFullScreen) { /* Firefox */
-                elem.mozRequestFullScreen();
-            } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-                elem.webkitRequestFullscreen();
-            } else if (elem.msRequestFullscreen) { /* IE/Edge */
-                elem.msRequestFullscreen();
-            }
-            $(this).hide().next('span').show();
-        });
-        $("#pdf-fullscreen-container").on('click','#able-exit-fullscreen',function() {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            } else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen();
-            } else if (document.webkitExitFullscreen) {
-                document.webkitExitFullscreen();
-            } else if (document.msExitFullscreen) {
-                document.msExitFullscreen();
-            }
-            $(this).hide().prev('span').show();
-        });
+        // $("#pdf-fullscreen-container").html('<span title="Full Screen" id="able-fullscreen">' +
+        //                                         '<img src="./assets/fullscreen.png">' +
+        //                                     '</span>' +
+        //                                     '<span title="Exit Full Screen" id="able-exit-fullscreen">' +
+        //                                         '<img src="./assets/exit-fullscreen.png">' +
+        //                                     '</span>');
+        // var elem = document.documentElement;
+        // $("#pdf-fullscreen-container").on('click','#able-fullscreen',function() {
+        //     if (elem.requestFullscreen) {
+        //         elem.requestFullscreen();
+        //     } else if (elem.mozRequestFullScreen) { /* Firefox */
+        //         elem.mozRequestFullScreen();
+        //     } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        //         elem.webkitRequestFullscreen();
+        //     } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        //         elem.msRequestFullscreen();
+        //     }
+        //     $(this).hide().next('span').show();
+        // });
+        // $("#pdf-fullscreen-container").on('click','#able-exit-fullscreen',function() {
+        //     if (document.exitFullscreen) {
+        //         document.exitFullscreen();
+        //     } else if (document.mozCancelFullScreen) {
+        //         document.mozCancelFullScreen();
+        //     } else if (document.webkitExitFullscreen) {
+        //         document.webkitExitFullscreen();
+        //     } else if (document.msExitFullscreen) {
+        //         document.msExitFullscreen();
+        //     }
+        //     $(this).hide().prev('span').show();
+        // });
         // Full Screen Icon Added
 
         // Add Zoom-in and Zoom-out Icons into the toolbar
